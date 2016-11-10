adf6d9373291f82c410506e673d7e8fd7811cd1e
Derek Hansen <hansenderek94@yahoo.com>
2016-11-10 15:13:12 -0800

    added boarders to the board

--- Pong/Pong.asm
+++ Pong/Pong.asm
@@ -6,7 +6,7 @@ INCLUDE Irvine32.inc
 
 COUNT DWORD ?
 
-Array2d		BYTE 100 DUP(' ')
+Array2d		BYTE 100 DUP('-')
 			BYTE 100 DUP(' ')
 			BYTE 100 DUP(' ')
 			BYTE 100 DUP(' ')
@@ -33,7 +33,7 @@ Array2d		BYTE 100 DUP(' ')
 			BYTE 100 DUP(' ')
 			BYTE 100 DUP(' ')
 			BYTE 100 DUP(' ')
-			BYTE 100 DUP(' ')
+			BYTE 100 DUP('-')
 
 ArraySize = 100
 NumArrays = 28
@@ -53,14 +53,18 @@ drawBoard proc
 	L1:
 		push ecx					;Maintain the state of ecx while we go into the next loop
 		mov ecx, ArraySize			;Loop through the size of the sub array
+		mov al, '|'
+		call WriteChar
 		L2:
 			mov al,Array2d+[esi]	;Mov value of Array2d into lower half of register eax
 			call WriteChar			;Write the character to the screen
 			inc esi					;This needs to only be incremented here because it's technically a linear array
 		LOOP L2
 		pop ecx						;Restore the state of ecx so we can continue looping through L1
-
+		mov al, '|'
+		call WriteChar
 		call crlf					;Get a new line
+
 	LOOP L1
 
 	;Restore the registers
